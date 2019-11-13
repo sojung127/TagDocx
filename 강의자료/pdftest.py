b9d8b213c5fbe89265e1c8832042491947dd0209
sojung127 <ssojung127@gmail.com>
2019-11-13 19:35:57 +0900

    강의자료 스코어링 1차

--- 강의자료/pdftest.py
+++ 강의자료/pdftest.py
@@ -18,7 +18,39 @@ def read_pdf_file(pdfFile):
     retstr.close()
     return content
 
-pdf_file = open("ESL_LN2.pdf", "rb")                                       # 로컬 PC에 있는 pdf 파일도 읽을 수 있음
+pdf_file = open("L8_SWDesign_Simulink_실습_s (1).pdf", "rb")                                       # 로컬 PC에 있는 pdf 파일도 읽을 수 있음
 contents = read_pdf_file(pdf_file)
-print(contents)
+
+lines=contents.split('\n')
+
+fp=open('ClassFeature.txt','r',encoding='utf-8-sig')
+features = fp.readlines()
+fp.close()
+
+scoreList=[]
+featureList=[]
+
+for i in range(len(features)):
+    scoreList.append(0)
+    features[i]=features[i].strip()
+    featureList.append(features[i].split())
+
+for i in range(len(lines)):
+    lines[i]=lines[i].replace(" ","")
+
+index=0
+
+sum=0
+value=1
+
+for i in range(len(featureList)):
+    for line in lines:
+        for w in featureList[i]:
+            if w in line.upper():
+                scoreList[index]=scoreList[index]+1*value
+                sum=sum+value
+    index=index+1
+
 pdf_file.close()
+
+print(scoreList)
