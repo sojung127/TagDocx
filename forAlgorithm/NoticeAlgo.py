a525837585d9521c6d39907b1a3feb8b387522f4
yjo5252 <gracejo0227@gmail.com>
2020-01-11 02:53:11 +0900

    Update Total_Scoring.py

--- forAlgorithm/NoticeAlgo.py
+++ forAlgorithm/NoticeAlgo.py
@@ -30,61 +30,75 @@ def convert_pdf_to_txt(path):
     retstr.close()
     return text
 
-path=input()
-lines=[]
-print(path[-3:]=='pdf')
-if path[-3:]=='pdf':
-    contents = convert_pdf_to_txt(path)
-   
-else:
-    fp = open(path,'r',encoding='utf-8')
-    contents=fp.readlines()
+import os
+
+path = "./"
+
+
+path_origin=input()
+file_list = os.listdir(path_origin)
+
+for i in range(5):
+    path=path_origin+file_list[i]
+    print(path)
+    lines=[]
+    print(path[-3:]=='pdf')
+    if path[-3:]=='pdf':
+        contents = convert_pdf_to_txt(path)
+    
+    elif path[-3:]=='txt':
+        fp = open(path,'r',encoding='utf-8')
+        contents=fp.readlines()
+        fp.close()
+    else:
+        continue
+    fp = open ('forAlgorithm/NoticeFeature.txt','r',encoding='utf-8-sig')
+    features = fp.readlines()
     fp.close()
-fp = open ('forAlgorithm/NoticeFeature.txt','r',encoding='utf-8-sig')
-features = fp.readlines()
-fp.close()
 
 
-scoreList=[]#10개
-featureList=[]
+    scoreList=[]#10개
+    featureList=[]
 
-for i in range(len(features)):
-    scoreList.append(0)
-    features[i]=features[i].strip()
-    featureList.append(features[i].split())
+    for i in range(len(features)):
+        scoreList.append(0)
+        features[i]=features[i].strip()
+        featureList.append(features[i].split())
 
-print(featureList)
 
-if path.find('슈퍼루키')>=0:
-    print(contents.count('\n'))
-    contents=contents.replace('\n','')
-print(contents.count('\n'))
-index=0
-isFind=False
-
-sum=0
-value=0
-
-for i in range(len(featureList)):
-    value=1
-    for w in featureList[i]:
-        scoreList[index]+=contents.count(w)
-        sum+=contents.count(w)
-        '''
-        if w in contents:
-            scoreList[index]=scoreList[index]+1*value
-            print(i)
-            print(w,value)
-            print(scoreList)
-             #합값생성
-            '''
-        
-    index=index+1
+    if path.find('슈퍼루키')>=0:
+        contents=contents.replace('\n','')
+    index=0
     isFind=False
 
-print()
-print(scoreList)
-print(sum)
+    sum=0
+    value=0
+
+    for i in range(len(featureList)):
+        value=1
+        for w in featureList[i]:
+            if path[-3:]=='txt':
+                for content in contents:
+                    scoreList[index]+=content.count(w)
+                    sum+=content.count(w)
+            else:
+                scoreList[index]+=contents.count(w)
+                sum+=contents.count(w)
+            '''
+            if w in contents:
+                scoreList[index]=scoreList[index]+1*value
+                print(i)
+                print(w,value)
+                print(scoreList)
+                #합값생성
+                '''
+            
+        index=index+1
+        isFind=False
+
+    print()
+    print(scoreList)
+    print(sum)
 
     
 
