6271854d4f3a2066b7663c52500f8e981d80d8f9
sojung127 <ssojung127@gmail.com>
2020-01-17 16:17:37 +0900

    현재 파일 읽은 진행도 표시, 제대로 나오지 않는 파일의 경로 출력

--- Scoring/Total_Scoring.py
+++ Scoring/Total_Scoring.py
@@ -173,10 +173,16 @@ def scoring(doc):
     #print(Score_list)
     global scan
     global score
-    print(Score_list.index(max(Score_list)))
+    global index
+    index+=1
+    global file_count
+    global path
+    print(index,file_count)
 
     if Score_list.index(max(Score_list))==scan:
         score+=1
+    else:
+        print(path)
 
 
 
@@ -184,6 +190,7 @@ def scoring(doc):
 #테스트 해볼 폴더의 분류 입력 [논문:0,기사:1,수업자료:2,공고:3,지원서:4]
 scan=int(input('입력 [논문:0,기사:1,수업자료:2,공고:3,지원서:4]'))
 score=0
+index=0
 path_origin = input("문서경로:")
 file_list = os.listdir(path_origin) #list 반환
 #list = ['공10.pdf', '공11.pdf', '공12.pdf', '공13.pdf', '공14.pdf', '공15.pdf', '공16.pdf', '공17.pdf', '공19.pdf', '공2.pdf', '공20.pdf', '공21.pdf', '공22.pdf', '공23.pdf', '공24.pdf', '공3.pdf', '공6.pdf', '공7.pdf', '공8.pdf', '공9.pdf']
