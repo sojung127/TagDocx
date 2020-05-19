d54bb75456933fff666d27dad554b365ee1f923b
yjo5252 <gracejo0227@gmail.com>
2020-05-19 23:12:40 +0900

    검색 화면 구성

--- TagDocx/SearchPage.xaml.cs
+++ TagDocx/SearchPage.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace TagDocx
 {
     /// <summary>
---
+++ TagDocx/SearchWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TagDocx
+{
+    /// <summary>
+    /// SearchWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class SearchWindow : Window
+    {
+        public SearchWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Listbox1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
