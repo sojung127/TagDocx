b2e88758edb261c5f3a09b27e5a30fe84f7ce3cb
sojung127 <ssojung127@gmail.com>
2020-05-30 13:51:56 +0900

    폴더 이동 테스트

--- userInterface/TagDocx/TagDocx/MainWindow.xaml.cs
+++ userInterface/TagDocx/TagDocx/MainWindow.xaml.cs
@@ -22,25 +22,46 @@ namespace TagDocx
     /// </summary>
     public partial class MainWindow : Window
     {
+        int moved;
+        string []fileList = new string[10];
         public MainWindow()
         {
             InitializeComponent();
+
+            moved = 0;
+            for(int i = 0; i < 10; i++)
+            {
+                fileList[i] = "";
+            }
             //FileSystemWatcher fsw = new FileSystemWatcher("C:\\AutomaticDocumentClassificationService\\Dataset\\한글\\기사\\IT과학\\");
             Console.WriteLine(Directory.GetCurrentDirectory()); // = TagDocx\bin\debug 
-            string path = System.IO.Path.GetFullPath(System.IO.Path.Combine(Directory.GetCurrentDirectory(), @"..\..\..\..\..\Dataset\"));
-            FileSystemWatcher fsw = new FileSystemWatcher(path);
+                                                                // dataset 상대경로
+                                                                //string path = System.IO.Path.GetFullPath(System.IO.Path.Combine(Directory.GetCurrentDirectory(), @"..\..\..\..\..\Dataset\"));
+
+            // test용 경로
+            string testPath = @"C:\test\";
+            string TempPath = @"C:\Temp\";
+            FileSystemWatcher fsw = new FileSystemWatcher(testPath);
+            FileSystemWatcher fswTemp = new FileSystemWatcher(TempPath);
 
             fsw.EnableRaisingEvents = true;
+            fswTemp.EnableRaisingEvents = true;
 
             //  Register a handler that gets called when a
             //  file is created, changed, or deleted.
             fsw.Changed += new FileSystemEventHandler(OnChanged);
 
-            fsw.Created += new FileSystemEventHandler(OnChanged);
+            fsw.Created += new FileSystemEventHandler(OnCreated);
 
-            fsw.Deleted += new FileSystemEventHandler(OnChanged);
+            fsw.Deleted += new FileSystemEventHandler(OnDeleted);
 
-           // fsw.Renamed += new FileSystemEventHandler(OnRenamed);
+            fswTemp.Changed += new FileSystemEventHandler(OnChanged);
+               
+            fswTemp.Created += new FileSystemEventHandler(OnCreated);
+               
+            fswTemp.Deleted += new FileSystemEventHandler(OnDeleted);
+
+            // fsw.Renamed += new FileSystemEventHandler(OnRenamed);
 
             Console.WriteLine("!!!!");
             MainHome.Content = new MainPage();
@@ -65,18 +86,59 @@ namespace TagDocx
         {
             Application.Current.Shutdown();
         }
-
-
-        private void watch()
+       void OnCreated(object soruce,FileSystemEventArgs e)
         {
+            Console.WriteLine("\n" + e.Name + "created start "+moved);
+            for(int i = moved-1; i >= 0; i--)
+            {
+                    Console.Write(fileList[i] + " " + i);
+                if (fileList[i].Equals(e.Name))
+                {
+                    // 이 경우 db에 추가
+                    fileList[i] = "";
+                    Console.WriteLine("일치");
+                    return;
+                }
 
-            string path = "C:\\AutomaticDocumentClassificationService\\Dataset\\한글\\기사\\IT과학\\";
-            FileSystemWatcher watcher = new FileSystemWatcher();
-            watcher.Path = path;
-            watcher.NotifyFilter = NotifyFilters.LastWrite;
-            watcher.Filter = "*.*";
-            watcher.Changed += new FileSystemEventHandler(OnChanged);
-            watcher.EnableRaisingEvents = true;
+            }
+            fileList[moved] = e.Name;
+            if (moved < 9)
+                moved++;
+            else
+                moved = 0;
+            Console.WriteLine("추가");
+
+            Console.WriteLine(
+                "Change noticed: Object Name = {0}, Object Event: {1} File Content",
+                e.Name, e.ChangeType);
+        }
+
+        void OnDeleted(object source, FileSystemEventArgs e)
+        {
+            Console.WriteLine("\n" + e.Name + "deleted start "+moved);
+            for (int i = moved-1; i >= 0; i--)
+            {
+                    Console.Write(fileList[i]+" "+i);
+                if (fileList[i].Equals(e.Name))
+                {
+                    // 이 경우 db에 추가
+                    fileList[i] = "";
+                    Console.WriteLine("일치");
+                    return;
+                }
+
+            }
+            fileList[moved] = e.Name;
+            if (moved < 9)
+                moved++;
+            else
+                moved = 0;
+            Console.WriteLine("스택 추가");
+
+            Console.WriteLine(
+                "Change noticed: Object Name = {0}, Object Event: {1} File Content",
+                e.Name, e.ChangeType);
+
         }
 
         void OnChanged(object source, FileSystemEventArgs e)
@@ -92,9 +154,7 @@ namespace TagDocx
             WatcherChangeTypes wct = e.ChangeType;
             Console.WriteLine("File {0} {1}", e.FullPath, wct.ToString());
             Console.WriteLine("변경!!!!");
-
-
-
+            
         }
 
         private static void OnRenamed(object source, RenamedEventArgs e)
